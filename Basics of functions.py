1e632d09c44e77bc4085bc6e2837fc6d364d59dd
Gilberto Granados <67038183+Gilberto-GZ@users.noreply.github.com>
2023-01-15 14:53:53 -0600

    Add files via upload

---
+++ Basics of functions.py
@@ -0,0 +1,19 @@
+
+
+
+def main():
+    
+    
+    x = int(input("What's X? "))
+    
+   
+    print("X squared is" , square(x))
+  
+def square(n):
+    return pow(n, 2)
+    
+    
+
+    
+main()
+
